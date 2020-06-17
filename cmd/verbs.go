abeceb3682f11dd840acd75430ee307d58695aee
Daniel Olsson <daniel.olsson@bisnode.com>
2020-06-17 12:47:17 +0200

    Added a version command

--- cmd/verbs.go
+++ cmd/verbs.go
@@ -16,6 +16,8 @@ limitations under the License.
 package cmd
 
 import (
+	"fmt"
+
 	"github.com/spf13/cobra"
 )
 
@@ -59,9 +61,25 @@ var createCmd = &cobra.Command{
 	},
 }
 
+// versionCmd represents the version command
+var versionCmd = &cobra.Command{
+	Use:   "version",
+	Short: "A brief description of your command",
+	Long: `A longer description that spans multiple lines and likely contains examples
+and usage of using your command. For example:
+
+Cobra is a CLI library for Go that empowers applications.
+This application is a tool to generate the needed files
+to quickly create a Cobra application.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		fmt.Printf("v%v\n", version)
+	},
+}
+
 func init() {
 	rootCmd.AddCommand(createCmd)
 	rootCmd.AddCommand(deleteCmd)
 	rootCmd.AddCommand(patchCmd)
 	rootCmd.AddCommand(getCmd)
+	rootCmd.AddCommand(versionCmd)
 }
